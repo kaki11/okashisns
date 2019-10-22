6816216805dad4459588f0de8236cf206487cc90
kakizakimaki <xxxmaki112@gmail.com>
2019-10-22 11:15:51 +0900

    add user show

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: with:/\A\S+@\S+\.\S+\z/ },
+                    format: { with: /\A\S+@\S+\.\S+\z/ },
                     # 大文字小文字を区別しない
                     uniqueness: { case_sensitive: false }
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
-
   get  '/signup',  to: 'users#new'
+  resources :users
 end
