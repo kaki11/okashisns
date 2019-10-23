75468d3fa71dff6dcb43f6070662bbc6076e5019
kakizakimaki <xxxmaki112@gmail.com>
2019-10-23 12:11:12 +0900

    add model micropost

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy #userと一緒に破棄される
+  default_scope -> { order(created_at: :desc) } #新しい投稿から並ぶ
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
