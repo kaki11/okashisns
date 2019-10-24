04678bc98b6e7da01f06200c67d03f1083d94566
kakizakimaki <xxxmaki112@gmail.com>
2019-10-24 12:48:25 +0900

    capistrano

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,5 @@
+server '3.115.165.219', user: 'ec2-user', roles: %w{app db web}
+
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
