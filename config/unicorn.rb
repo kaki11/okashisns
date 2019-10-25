9fe5f7568605d227f1d46b39827dbcaf1eef77af
kakizakimaki <xxxmaki112@gmail.com>
2019-10-25 11:43:27 +0900

    pid パス変更

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,5 +1,6 @@
 #サーバ上でのアプリケーションコードが設置されているディレクトリを変数に入れておく
 app_path = File.expand_path('../../../', __FILE__)
+shared_path = File.expand_path('../../../shared/', __FILE__)
 
 #アプリケーションサーバの性能を決定する
 worker_processes 1
@@ -10,7 +11,7 @@ working_directory "#{app_path}/current"
 listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 
 #Unicornの起動に必要なファイルの設置場所を指定
-pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+pid "#{shared_path}/tmp/pids/unicorn.pid"
 
 #ポート番号を指定
 listen "#{app_path}/tmp/sockets/unicorn.sock"
