480d973f92c8159b4ed8551b835e90018ea3be44
kakizakimaki <xxxmaki112@gmail.com>
2019-10-25 11:18:18 +0900

    set credentials

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,6 +10,7 @@ set :repo_url,  'git@github.com:kaki11/okashisns.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+set :linked_files, fetch(:linked_files, []).push("config/master.key")
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1'  # バージョン
