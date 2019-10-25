2828362dba87a8ce1dbf43b05091dbc0ed06eb34
kakizakimaki <xxxmaki112@gmail.com>
2019-10-25 16:10:38 +0900

    CarrierWave setting

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,7 @@ require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
-  if Rails.env.development? || Rails.env.test? #開発とテストは今まで通りに
-    config.storage = :file
-  elsif Rails.env.production? #本番はS3に保存する
+  if Rails.env.production? #本番はS3に保存する
     config.storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -16,5 +14,8 @@ CarrierWave.configure do |config|
     }
     config.fog_directory  = 'okashisns'
     config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/okashisns'
+  else
+      config.storage = :file
+      config.enable_processing = false if Rails.env.test?
   end
 end
