8402dd27e1a4a37a7a614ff854a0a5d7a90528b9
kakizakimaki <xxxmaki112@gmail.com>
2019-10-25 10:23:45 +0900

    s3の導入

---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,17 @@
+require 'carrierwave/storage/abstract'
+require 'carrierwave/storage/file'
+require 'carrierwave/storage/fog'
+
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.credentials.aws_access_key_id,
+    aws_secret_access_key: Rails.application.credentials.aws_secret_access_key,
+    region: 'ap-northeast-1'
+  }
+
+  config.fog_directory  = 'okashisns'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/okashisns'
+end
