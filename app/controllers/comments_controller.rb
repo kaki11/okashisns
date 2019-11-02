530ec73ac45ad742b2acde76e1571b0bb65e7781
kakizakimaki <xxxmaki112@gmail.com>
2019-11-02 19:50:25 +0900

    保存できない場合の処理

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,17 +4,20 @@ class CommentsController < ApplicationController
     @micropost = Micropost.find_by(params[:id])
     if current_user
       @comment = current_user.comments.build(comment_params)
-      if @comment.save!
-        redirect_back(fallback_location: root_path)
-      else
-        redirect_back(fallback_location: root_path)
-      end
-    else
-      
       if @comment.save
         redirect_back(fallback_location: root_path)
       else
         redirect_back(fallback_location: root_path)
+        flash[:danger] = "こめんとがないよ＞＜"
+      end
+    else
+      
+      @comment = Comment.new(comment_params)
+      if @comment.save!
+        redirect_back(fallback_location: root_path)
+      else
+        redirect_back(fallback_location: root_path)
+        flash[:danger] = "こめんとがないよ＞＜"
       end
     end
 
