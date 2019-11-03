e7d691a0b7ec4b28d27fdbf72a2b14670894d06d
kakizakimaki <xxxmaki112@gmail.com>
2019-11-03 11:35:27 +0900

    dependent: :destroy

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   has_many :favorites
   has_many :favorite_users, through: :favorites, source: 'user'
-  has_many :comments
+  has_many :comments, dependent: :destroy
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
