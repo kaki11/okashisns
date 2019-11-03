4ae8855ad450ae42b322ac004fb31d8ac3621b40
kakizakimaki <xxxmaki112@gmail.com>
2019-11-03 11:33:11 +0900

    コメント削除の条件分岐

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,4 +1,5 @@
 class CommentsController < ApplicationController
+
   def create
     # hidden_fieldの値を受け取る
     @micropost = Micropost.find_by(params[:id])
@@ -11,7 +12,6 @@ class CommentsController < ApplicationController
         flash[:danger] = "こめんとがないよ＞＜"
       end
     else
-      
       @comment = Comment.new(comment_params)
       if @comment.save
         redirect_back(fallback_location: root_path)
@@ -20,11 +20,24 @@ class CommentsController < ApplicationController
         flash[:danger] = "こめんとがないよ＞＜"
       end
     end
+  end
 
+  def destroy
+    @comment = Comment.find(params[:id])
+    @micropost = Micropost.find_by(user_id:current_user.id)
+    if @micropost.user_id == current_user.id
+      @comment.destroy
+      flash[:success] = "削除しました"
+      redirect_back(fallback_location: root_path)
+    else
+      redirect_back(fallback_location: root_path)
+      flash[:danger] = "削除できません＞＜"
+    end
   end
 
   private
   def comment_params
     params.require(:comment).permit(:content, :micropost_id)
   end
+
 end
