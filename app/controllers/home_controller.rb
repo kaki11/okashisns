dec90d745d308ee5f263c3add1d076b52f99b919
kakizakimaki <xxxmaki112@gmail.com>
2019-11-04 14:35:05 +0900

    人気レシピの表示

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,18 +1,21 @@
 class HomeController < ApplicationController
   def index
     @microposts = Micropost.all
-    ranking_rakuten_api
+    sweets_ranking_api
+    pan_ranking_api
+
   end
 
-  def ranking_rakuten_api
+  def sweets_ranking_api
+    res = RakutenWebService::Recipe.ranking(category_id = 21)
+    # 取得したデータを配列に格納し直す
+    @sweets_recipes = res.map{|recipe| recipe}
+  end
 
-    categories = RakutenWebService::Recipe.ranking
-      categories.each do |category| 
-        @imageUrl = category['foodImageUrl'] 
-        @recipeTitle = category['recipeTitle'] 
-        @recipeUrl = category['recipeUrl'] 
-      end
-
+  def pan_ranking_api
+    res = RakutenWebService::Recipe.ranking(category_id = 22)
+    # 取得したデータを配列に格納し直す
+    @pan_recipes = res.map{|recipe| recipe}
   end
 
 end
