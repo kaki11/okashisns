5860496e7fe163a6dbc0679363ff3d7aafc2504a
kakizakimaki <xxxmaki112@gmail.com>
2019-11-04 12:27:27 +0900

    api導入

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,18 @@
 class HomeController < ApplicationController
   def index
     @microposts = Micropost.all
+    ranking_rakuten_api
   end
+
+  def ranking_rakuten_api
+
+    categories = RakutenWebService::Recipe.ranking
+      categories.each do |category| 
+        @imageUrl = category['foodImageUrl'] 
+        @recipeTitle = category['recipeTitle'] 
+        @recipeUrl = category['recipeUrl'] 
+      end
+
+  end
+
 end
--- config/initializers/rakuten.rb
+++ config/initializers/rakuten.rb
@@ -1,3 +1,4 @@
-RakutenWebService.configuration do |c|
+RakutenWebService.configure do |c|
     c.application_id = '1077013442292739292'
-end
+    c.affiliate_id = '198877a3.61d69a52.198877a4.abcbab03'
+  end
