a3236e0760e3970444fc72dcf85a8153bf657a61
kakizakimaki <xxxmaki112@gmail.com>
2019-11-08 18:47:29 +0900

    add controller relationships

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,12 @@ class User < ApplicationRecord
   has_many :favorites
   has_many :favorite_microposts, through: :favorites, source: 'micropost'
   has_many :comments
+
+  has_many :relationships
+  has_many :followings, through: :relationships, source: :follow
+  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  has_many :followers, through: :reverse_of_relationships, source: :user
+
   default_scope -> { order(created_at: :desc) } #新しい投稿から並ぶ
   before_save { self.email = email.downcase }
   validates :name, presence: true, length: { maximum: 50 }
@@ -13,4 +19,24 @@ class User < ApplicationRecord
 
   has_secure_password
   validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
+
+
+  def follow(other_user)
+    unless self == other_user
+      self.relationships.find_or_create_by(follow_id: other_user.id)
+    end
+  end
+
+  def unfollow(other_user)
+    relationship = self.relationships.find_by(follow_id: other_user.id)
+    relationship.destroy if relationship
+  end
+
+  def following?(other_user)
+    self.followings.include?(other_user)
+  end
+
+
+
+
 end
