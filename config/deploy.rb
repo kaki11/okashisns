17d5a9058de35f67fc2a8f82b5728689532aadbf
kakizakimaki <xxxmaki112@gmail.com>
2019-11-08 18:32:43 +0900

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,7 +15,7 @@ set :rbenv_ruby, '2.5.1'
 
 # どの公開鍵を利用してデプロイするか
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['/.ssh/okasisnsmaki.pem'] 
+                keys: ['~/.ssh/okasisnsmaki.pem'] 
 
 # プロセス番号を記載したファイルの場所
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
