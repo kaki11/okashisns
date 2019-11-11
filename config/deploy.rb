0e1f010af9cc00391c93e204c0d68f5c09a1551e
kakizakimaki <xxxmaki112@gmail.com>
2019-11-11 14:32:07 +0900

    db_seed

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,6 +27,17 @@ set :keep_releases, 5
 # デプロイ処理が終わった後、Unicornを再起動するための記述
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
+  desc 'db_seed'
+  task :db_seed do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:seed'
+        end
+      end
+    end
+  end
+  
   task :restart do
     invoke 'unicorn:restart'
   end
