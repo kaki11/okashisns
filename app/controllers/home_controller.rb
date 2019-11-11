90bef7c51fc54d6a45dba9f9a71f5d1460335043
kakizakimaki <xxxmaki112@gmail.com>
2019-11-11 15:10:47 +0900

    sidebarの編集

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,8 @@
 class HomeController < ApplicationController
   def index
+    @sweets_categories = Category.limit(10)
+    @bread_categories = Category.limit(7).offset(10)
+    @other_categories = Category.offset(17)
     @microposts = Micropost.all.limit(8)
     recipe_ranking_api
   end
