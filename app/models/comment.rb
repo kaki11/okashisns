bcf4a78c397bfd39e9276be713a4c379c9f13bd6
kakizakimaki <xxxmaki112@gmail.com>
2019-11-14 17:23:26 +0900

    画像の表示修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,4 +4,16 @@ class Comment < ApplicationRecord
   validates :micropost_id, presence: true
   validates :content, presence: true, length: { maximum: 200 }
   default_scope -> { order(created_at: :desc) }
+  validate :no_4_bytes
+
+  private
+    # 本文に4byte文字を使わないように
+    def no_4_bytes
+        if content.present?
+            chars = content.each_char.select{|c| c.bytes.count >= 4}
+            if chars.size > 0
+                errors.add(:content, "に絵文字(#{chars.join('')})は使用できません。")
+            end
+        end
+    end
 end
